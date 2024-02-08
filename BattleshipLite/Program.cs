bb84c54dbd1592208205195d5017ff403fbb4a8b
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 11:37:38 -0500

    Create Code
    Create a Console project in our solution and add in the methods the Console App will need.

--- BattleshipLite/Program.cs
+++ BattleshipLite/Program.cs
@@ -4,12 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BattleshipLibrary.Models;
+
 namespace BattleshipLite
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			_ = Console.ReadLine();
+		}
+
+		private static string GetPlayerName()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static List<GridSpotModel> GetShipPlacement()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static bool ValidShipSpot(GridSpotModel gridSpot)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void DisplayGrid(List<GridSpotModel> gridSpotModels)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static GridSpotModel GetFiredShot()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static bool ValidFiredShot(GridSpotModel gridSpot, List<GridSpotModel> gridSpotModels)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static GRIDSPOTSTATUS ShotOutcome(GridSpotModel gridSpot, List<GridSpotModel> gridSpotModels)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void DisplayScore()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void DisplayWinner()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
