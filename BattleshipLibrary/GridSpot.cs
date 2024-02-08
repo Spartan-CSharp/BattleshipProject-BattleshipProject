c2b4b662cac22edda0b20f5e44e7529ba2176452
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 11:28:00 -0500

    Create Code
    Create a Class Library with our data models. After we have completed it, we can compare it to what we do in the next lesson.

---
+++ BattleshipLibrary/GridSpot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLibrary
+{
+	public class GridSpot
+	{
+		public string SpotLetter
+		{
+			get; set;
+		}
+		public int SpotNumber
+		{
+			get; set;
+		}
+		public string Status
+		{
+			get; set;
+		}
+	}
+}
---
+++ BattleshipLibrary/PlayerModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLibrary
+{
+	public class PlayerModel
+	{
+		public string PlayerName
+		{
+			get; set;
+		}
+		public List<GridSpot> ShipLocations
+		{
+			get; set;
+		}
+		public List<GridSpot> ShotGrid
+		{
+			get; set;
+		}
+		public PlayerModel()
+		{
+			ShipLocations = new List<GridSpot>();
+			ShotGrid = new List<GridSpot>();
+		}
+	}
+}
